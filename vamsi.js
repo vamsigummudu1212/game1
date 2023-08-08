1aabcbdac6609fac83542c5159994e9c3a8edce2
vamsigummudu1212 <141009688+vamsigummudu1212@users.noreply.github.com>
2023-08-08 18:41:23 +0530

    Add files via upload

---
+++ vamsi.js
@@ -0,0 +1,21 @@
+var randomNumber=Math.floor(Math.random()*100)+1;
+var attempts=0;
+
+document.getElementById('btn').addEventListener('click',function(){
+    var guess=parseInt(document.getElementById('guessInput').value);
+    attempts++;
+    if(guess===randomNumber){
+         displayMessage("Congratulations!You guessed the Number in " + attempts + " attempt 🎉");
+         document.getElementById('btn').disabled=true;
+    }
+    else if(guess<randomNumber){
+        displayMessage("Too low! try a higher number 😪");
+    }
+    else{
+        displayMessage("Too high! Try a lower Number 😪")
+    }
+
+});
+function displayMessage(Message){
+    document.getElementById('msg').textContent=Message;
+}
